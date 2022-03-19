00e4b4a4664eb0a8b2ef38c8cfed194da15051ab
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-03-19 14:31:33 +0100

    added json test file

--- src/pages/calendar/Calendar.js
+++ src/pages/calendar/Calendar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './calendar.scss';
+import dataJSON from '../../test.json'
 
 const Calendar = () => {
 
-    const fetchData = () => {
+    // const fetchData = () => {
 
-        const baseURL = ''; 
-    }
+    //     const baseURL = process.env.REACT_APP_UPCOMING_CHANGES;
+    //     fetch(baseURL, {
+    //         mode: 'no-cors'
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //     .catch(error => console.error(error));
+    // }
+
+    // fetchData()
+
+    const data = dataJSON;
+    console.log(data)
 
     return(
         <>
